b2caa8be946ddb49f23c5bc9b7bca14a04c0508d
houstonlucas <houstonlucas@nevada.unr.edu>
2015-10-24 19:45:44 -0700

    Added Connection detection.

--- vidTest.py
+++ vidTest.py
@@ -1,35 +1,37 @@
 import numpy as np
 import cv2
-import copy
-import time
-import math
+import datetime
 from Tangrams import *
 
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture( 0 )
 
     pieces = []
+    connections = []
     frameCount = 0
-    while(True):
+    while( True ):
         # Capture frame-by-frame
         ret, img = cap.read()
 
-        valid = findValid(img)
-
         if frameCount%20 == 0:
+            valid = findValid(img)
             pieces = indentifyPieces(valid, img)
+            connections = findConnections(pieces)
 
-        blank = np.zeros(img.shape)
+        blank = np.zeros( img.shape )
 
-        drawPieces(blank, pieces)
-        #drawPieces(img, pieces)
+        drawPieces( blank, pieces )
+        drawConnections(img, connections)
 
 
         # Display the resulting frame
-        cv2.imshow('blank', blank)
-        cv2.imshow('frame',img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow( 'blank', blank )
+        cv2.imshow( 'frame', img )
+        currentWaitKey = cv2.waitKey( 1 ) & 0xFF
+        if currentWaitKey == ord( 'q' ):
             break
+        if currentWaitKey == ord( 'r' ):
+            writeStateToFile( pieces, connections )
 
     # When everything is done, release the capture
     cap.release()
@@ -42,6 +44,17 @@ def main():
 ###########################################
 ###########################################
 
+def writeStateToFile( pieces, connections ):
+    currentTime = str( datetime.datetime.now() )[:-7]
+    outfile = open( "state_" + currentTime, 'w+' )
+    for piece in pieces:
+        print "meow"
+        outfile.write(piece.toString() + '\n')
+    outfile.write('\n')
+    for connection in connections:
+        outfile.write(connection.toString() + '\n')
+
+    outfile.close()
 
 
 if __name__ == "__main__":
